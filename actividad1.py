6184060b9c56b1d4a13ea0a43f66ec5ade358c62
Aileen127 <fleuraileen394@gmail.com>
2025-07-15 09:27:19 -0600

    corrección variable de nombre

--- actividad1.py
+++ actividad1.py
@@ -1,8 +1,8 @@
 print("Hola primer Git")
 nombre = input("Ingresa tu nombre: ")
-edad = int(input("ingresa tu edad por favor"))
+edad = int(input("ingresa tu edad por favor: "))
 print("Bienvenido")
-print(f"tu nombre es {hola}")
+print(f"tu nombre es {nombre}")
 if edad > 18:
     print("Eres mayor de edad")
 elif edad > 0:
