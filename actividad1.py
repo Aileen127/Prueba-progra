9198df81d1d4a5bf07386bd6a321bda1d77c03d8
Aileen127 <fleuraileen394@gmail.com>
2025-07-15 09:37:15 -0600

    Elaboración de cadena if acerca de la facultad a la que pertenece

--- actividad1.py
+++ actividad1.py
@@ -1,11 +1,17 @@
 print("Hola primer Git")
 nombre = input("Ingresa tu nombre: ")
-edad = int(input("ingresa tu edad por favor: "))
+edad = int(input("Ingresa tu edad por favor: "))
+Facultad = input("Ingresa la facultad a la que perteneces ").lower()
 print("Bienvenido")
 print(f"tu nombre es {nombre}")
-if edad > 18:
+if edad  >= 18:
     print("Eres mayor de edad")
 elif edad > 0:
     print("Eres menor de edad")
 else:
     print("Edad ingresada incorrectamente.")
+
+if Facultad == "ingenieria":
+    print("Bienvenido a la facultad de ingenieria")
+elif Facultad != "ingenieria":
+    print("Usted es de una facultad distinta")
